525a968a4776b394ebb74500cef6b39d2dcf8673
akaruijun <patrickjun2208@gmail.com>
2024-09-19 15:08:13 -0300

    Add Drizzle and Neon

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,16 @@
-import { z } from 'zod'
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { zValidator } from '@hono/zod-validator'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
 
-export const runtime = 'edge'
+import accounts from "./accounts";
+
+export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-  return c.json({ hello: "World" });
-});
+const routes = app
+  .route("/accounts", accounts);
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type AppType = typeof routes;
